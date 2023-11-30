878cd715ca23da9820e41c71c7806646b39fadbd
ismayilhsnv <tu7u02d2s@code.edu.az>
2023-11-30 13:33:14 +0400

    Nazile was created 3 section

--- cafe/src/components/bestCoffe/index.jsx
+++ cafe/src/components/bestCoffe/index.jsx
@@ -2,37 +2,24 @@ import React from 'react'
 import './bestCoffe.scss'
 const BestCoffee = () => {
     return (
-        <section>
-            <div className='areaBestCoffee'>
-                <div className='bestCoffeeLeft'>
-                    <div className='img1'>
-                        <img src="https://xpressrow.com/html/cafena/cafena/assets/images/shape/about-shape-2-1.png" alt="" />
-                    </div>
-                </div>
-                <div className='bestCoffeeRight'>
-                    <div className='img2'>
-                        <img src="https://xpressrow.com/html/cafena/cafena/assets/images/shape/about-shape-2-2.png" alt="" />
-                    </div>
-                </div>
-                <div className='coffeeContainer'>
-                    <div className='aboutLeft'>
-
+        <section id='Best'>
+            <div className='aboutArea'>
+                <div className='areaLeft'>
+                    <div className='leftImage'>
                         <img className='bigImage' src="https://xpressrow.com/html/cafena/cafena/assets/images/about/about-img-2-1.png" alt="" />
-                        <img className='smallImg' src="https://xpressrow.com/html/cafena/cafena/assets/images/about/about-img-2-2.png" alt="" />
+                        <img className='smallImage' src="https://xpressrow.com/html/cafena/cafena/assets/images/about/about-img-2-2.png" alt="" />
                     </div>
-                    <div className='coffeRightText'>
-                        <h6 className='abouth6'>
-                            ABOUT US
-                        </h6>
-                        <h1>ONE OF THE BEST COFFEE <br />
-                         HOUSE IN YOUR HOME TOWN</h1>
-                         <p>Mauris rhoncus orci in imperdiet placerat. Vestibulum euismod <br /> nisl suscipit ligula volutpat, a feugiat urna maximus. Cras massa <br /> nibhtincidunt.</p> <br />
-                         <p>Donec et nibh maximus, congue est eu, mattis nunc. Praesent ut <br /> quam quis quam venenatis fringilla. Morbi vestibulum id tellus <br /> mmodo mattis. Aliquam erat volutpat. Aenean accumsan id mi <br />nec semper. Lorem ipsum.</p>
+                </div>
+                <div className='areaRight'>
+                    <div className='textRight'>
+                        <div className='aboutUS'><h6>ABOUT US</h6></div>
+                        <h1>ONE OF THE BEST COFFEE HOUSE IN YOUR HOME TOWN</h1>
+                        <p>Mauris rhoncus orci in imperdiet placerat. Vestibulum euismod nisl suscipit ligula volutpat, a feugiat urna maximus. Cras massa  nibhtincidunt.</p><br />
 
+                        <p>Donec et nibh maximus, congue est eu, mattis nunc. Praesent ut quam quis quam venenatis fringilla. Morbi vestibulum id tellus mmodo mattis. Aliquam erat volutpat. Aenean accumsan id mi nec  semper. Lorem ipsum.</p>
                     </div>
                 </div>
             </div>
-
         </section>
     )
 }
--- cafe/src/components/grade/index.jsx
+++ cafe/src/components/grade/index.jsx
@@ -2,72 +2,64 @@ import React from 'react'
 import './grade.scss'
 const Grade = () => {
     return (
-        <section>
-            <div className='gradeArea'>
-                <div className='topGradeLeft'>
-                    <div className='img1'>
-                        <img src="https://xpressrow.com/html/cafena/cafena/assets/images/shape/top-grade-shape-2-1.png" alt="" />
+        <section id='Grade'>
+        <div className='areaGrade'>
+            <div className='titleGrade'>
+                <div className='topGrade'>
+            <h6>
+                    TOP GRADE
+                </h6>
+                </div>
+                <h1>WORLD TOP GRADE</h1>
+            </div>
+            <div className='containerGrade'>
+            <div className='topGrade1'>
+                    <div className='gradeImg'>
+                        <img src="https://xpressrow.com/html/cafena/cafena/assets/images/top-grade/tg-2-1.jpeg" alt="" />
+                        <div className='count1'><h6>01</h6></div>
+                    </div>
+                    <div className='text'>
+                        <h2>Americano Grade</h2>
+                        <p>Curabitur semper erat a lacusey <br />
+                            consequat, sit amet quam</p>
                     </div>
                 </div>
-                <div className='topGradeRight'>
-                    <div className='img2'>
-                        <img src="https://xpressrow.com/html/cafena/cafena/assets/images/shape/top-grade-shape-2-2.png" alt="" />
+                <div className='topGrade2'>
+                    <div className='gradeImg'>
+                        <img src="https://xpressrow.com/html/cafena/cafena/assets/images/top-grade/tg-2-2.jpeg" alt="" />
+                        <div className='count2'><h6>02</h6></div>
+                    </div>
+                    <div className='text'>
+                        <h2>Arebica Green</h2>
+                        <p>Curabitur semper erat a lacusey <br />
+                            consequat, sit amet quam</p>
                     </div>
                 </div>
-                <div className='gradeTitle'>
-                    <h6 className='abouth6'>
-                        ABOUT US
-                    </h6>
-                    <h1>WORLD TOP GRADE</h1>
+                <div className='topGrade1'>
+                    <div className='gradeImg'>
+                        <img src="https://xpressrow.com/html/cafena/cafena/assets/images/top-grade/tg-2-3.jpeg" alt="" />
+                        <div className='count1'><h6>03</h6></div>
+                    </div>
+                    <div className='text'>
+                        <h2>Mixed Rosted</h2>
+                        <p>Curabitur semper erat a lacusey <br />
+                            consequat, sit amet quam</p>
+                    </div>
                 </div>
-                <div className='containerGrade'>
-                    <div className='topGrade1'>
-                        <div className='gradeImg'>
-                            <img src="https://xpressrow.com/html/cafena/cafena/assets/images/top-grade/tg-2-1.jpeg" alt="" />
-                            <div className='count1'><h6>01</h6></div>
-                        </div>
-                        <div className='text'>
-                            <h2>Americano Grade</h2>
-                            <p>Curabitur semper erat a lacusey <br />
-                                consequat, sit amet quam</p>
-                        </div>
+                <div className='topGrade2'>
+                    <div className='gradeImg'>
+                        <img src="https://xpressrow.com/html/cafena/cafena/assets/images/top-grade/tg-2-4.jpeg" alt="" />
+                        <div className='count2'><h6>04</h6></div>
                     </div>
-                    <div className='topGrade2'>
-                        <div className='gradeImg'>
-                            <img src="https://xpressrow.com/html/cafena/cafena/assets/images/top-grade/tg-2-2.jpeg" alt="" />
-                            <div className='count2'><h6>02</h6></div>
-                        </div>
-                        <div className='text'>
-                            <h2>Arebica Green</h2>
-                            <p>Curabitur semper erat a lacusey <br />
-                                consequat, sit amet quam</p>
-                        </div>
-                    </div>
-                    <div className='topGrade1'>
-                        <div className='gradeImg'>
-                            <img src="https://xpressrow.com/html/cafena/cafena/assets/images/top-grade/tg-2-3.jpeg" alt="" />
-                            <div className='count1'><h6>03</h6></div>
-                        </div>
-                        <div className='text'>
-                            <h2>Mixed Rosted</h2>
-                            <p>Curabitur semper erat a lacusey <br />
-                                consequat, sit amet quam</p>
-                        </div>
-                    </div>
-                    <div className='topGrade2'>
-                        <div className='gradeImg'>
-                            <img src="https://xpressrow.com/html/cafena/cafena/assets/images/top-grade/tg-2-4.jpeg" alt="" />
-                            <div className='count2'><h6>04</h6></div>
-                        </div>
-                        <div className='text'>
-                            <h2>Robasta Rosted</h2>
-                            <p>Curabitur semper erat a lacusey <br />
-                                consequat, sit amet quam</p>
-                        </div>
+                    <div className='text'>
+                        <h2>Robasta Rosted</h2>
+                        <p>Curabitur semper erat a lacusey <br />
+                            consequat, sit amet quam</p>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
     )
 }
 
